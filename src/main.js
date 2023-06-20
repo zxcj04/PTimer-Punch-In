da247db368ecca6e30c8e672a44ea02aae11f628
Fanrende <zxcj04@gmail.com>
2023-06-20 21:28:10 +0800

    chore: vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import vuetify from './plugins/vuetify'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(vuetify)
+app.mount('#app')
