5fbae8bbb3dd8bf6f6e3ac1389fa839f940b7592
Fanrende <zxcj04@gmail.com>
2023-06-20 22:11:07 +0800

    chore: vue cookies

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,18 @@
 <script setup>
 import apis from '@/apis/auth';
+import { useCookies } from "vue3-cookies";
 
 const login = async () => {
-  const { data } = await apis.userLogin();
-  console.log(data)
+  const { data } = await apis.userLogin({
+    mail: "acl",
+    password: "acl"
+  });
+  console.log(data);
+  const session_id = data.session_id;
+
+  const { cookies } = useCookies();
+
+  cookies.set("session_id", session_id);
 }
 </script>
 
